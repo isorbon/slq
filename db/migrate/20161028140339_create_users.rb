e0b10b41fa9cc3bceef5d5e9fb8f81bb0e8f6b7f
isorbon <clogsj@gmail.com>
2016-10-28 17:15:36 +0300

    migrate

--- db/migrate/20161028140339_create_users.rb
+++ db/migrate/20161028140339_create_users.rb
@@ -1,14 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :id
       t.text :f_name
       t.text :l_name
       t.text :login
       t.text :password
       t.text :img
       t.integer :type
-
       t.timestamps
     end
   end
